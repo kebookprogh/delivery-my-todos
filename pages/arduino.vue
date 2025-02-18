ffb6629892c34bf1c591704c4818a06160440a49
dev-kebook <kebook.programacao.12@gmail.com>
2025-02-18 15:14:58 -0300

    fixing ardus page

--- pages/arduino.vue
+++ pages/arduino.vue
@@ -1,13 +1,13 @@
 <script setup lang="ts">
 import TeacherImg from '@/components/mol/teacherImg/index.vue'
-import CarouselSpy from '@/components/organism/carouselSpy/index.vue'
+import CarouselArdu from '@/components/organism/carouselArdu/index.vue'
 
 </script>
 
 <template>
     <section class="background flex items-center justify-center space-x-14 h-[100vh]">
         <TeacherImg :teacherImg="'https://kronus.kebook.com.br/assets/6bf14529-ba27-4726-b0ec-a9149a96051e/expert-top.png.webp?format=webp&quality=80'"/>
-        <CarouselSpy />
+        <CarouselArdu />
         <TeacherImg :teacherImg="'https://kronus.kebook.com.br/assets/6bf14529-ba27-4726-b0ec-a9149a96051e/expert-top.png.webp?format=webp&quality=80'"/>
     </section>
 </template>
@@ -15,5 +15,6 @@ import CarouselSpy from '@/components/organism/carouselSpy/index.vue'
 <style scoped>
 .background {
     background-image: url(https://kronus.kebook.com.br/assets/4263e51d-c53a-48bf-848e-ccdb1ed5455c/back-top-w.png);
+    background-size: cover;
 }
 </style>
